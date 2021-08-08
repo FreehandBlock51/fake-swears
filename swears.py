ccd970fa708cc97608561c895fe3cf340023a645
superpotato9 <50345854+superpotato9@users.noreply.github.com>
2021-08-08 16:54:59 -0700

    added exit

--- swears.py
+++ swears.py
@@ -2,6 +2,7 @@ fname = ''
 wcount = 100
 mins = 4
 maxs = 8
+contu = True
 print('''
 welcome to:
 
@@ -48,7 +49,8 @@ while True:
   this is some simple code that uses fictionary to generate random words in this
   case based on and fine tuned for swears but will work with any text document
   to setup put your text document name on the commented line about the txt file
-  and make sure they are both in the same folder 
+  and make sure they are both in the same folder
+  
   command = 'run' run the code with set params 
   command = 'min' this changes min size of the words outputed
   command = 'max' this changes max size of the words outputed
@@ -78,6 +80,10 @@ while True:
     lines = a_file.readlines()
     for line in lines:
       print(line)
+  elif inpu == 'exit':
+    contu = False
+    break
+  
   else:
     print("error cmd does not exist")
   
@@ -87,42 +93,43 @@ while True:
 
 
 
-import fictionary 
-#this block of code feeds the txt line by line into the file converter and defines a few vars
-m = fictionary.Model()
-final_list = []
-words = open(fname, 'r').read()
-for word in words.splitlines():
-  m.feed(word)
+import fictionary
+while contu:
+  #this block of code feeds the txt line by line into the file converter and defines a few vars
+  m = fictionary.Model()
+  final_list = []
+  words = open(fname, 'r').read()
+  for word in words.splitlines():
+    m.feed(word)
 
-#this converts the file 
-with open('words.json', 'w', encoding='utf-8') as json:
-  m.write(json)
-with open("words.txt", 'r') as file_handle:
-  lines = file_handle.read().splitlines()
+  #this converts the file 
+  with open('words.json', 'w', encoding='utf-8') as json:
+    m.write(json)
+  with open("words.txt", 'r') as file_handle:
+    lines = file_handle.read().splitlines()
 
-#train it!
-new_model = fictionary.Model(words = ["a"])
-with open('words.json', 'r', encoding='utf-8') as fp:
-    new_model.read(fp)
-word = ""
-#get a bunch of random words but make sure it does not print any repeats
-print("running...")
-for i in range(1, wcount):#change this to have it make more words but does raise your On by a lot
-  word = m.random_word(mins, maxs)#change words length
-  final_list.append(word)
-  
+  #train it!
+  new_model = fictionary.Model(words = ["a"])
+  with open('words.json', 'r', encoding='utf-8') as fp:
+      new_model.read(fp)
+  word = ""
+  #get a bunch of random words but make sure it does not print any repeats
+  print("running...")
+  for i in range(1, wcount):#change this to have it make more words but does raise your On by a lot
+    word = m.random_word(mins, maxs)#change words length
+    final_list.append(word)
+    
 
-print("""
+  print("""
 
 
-""")
-final_list = set(final_list)
-final_list = list(final_list)
-for x in final_list:
-  print(x)
+  """)
+  final_list = set(final_list)
+  final_list = list(final_list)
+  for x in final_list:
+    print(x)
 
-input("press enter to exit")
+  input("press enter to exit")
 
 
 
